41d1560572828eb7c4e704e736083474432ea09f
Pedro Medeiros <pietromaddera@gmail.com>
2024-11-16 23:35:26 +0100

    Implemented Rest Endpoints

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -5,19 +5,29 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    public Beer getBearById(UUID id){
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
-        log.debug("Get Beer by Id - id: " + id.toString());
-        return beerService.getBeerById(id);
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBearById(@PathVariable("beerId") UUID beerId){
+        log.debug("Get Beer by Id - id: {}", beerId.toString());
+        return beerService.getBeerById(beerId);
     }
 }
