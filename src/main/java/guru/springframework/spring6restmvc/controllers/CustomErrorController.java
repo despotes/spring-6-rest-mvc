184fc6beb6dcb71f8724b142c2653acf96f1f81f
Pedro Medeiros <pietromaddera@gmail.com>
2024-11-19 18:19:08 +0100

    JPA Validation Error Message

--- src/main/java/guru/springframework/spring6restmvc/controllers/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/CustomErrorController.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring6restmvc.controllers;
 
 
+import jakarta.persistence.RollbackException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -16,7 +18,18 @@ public class CustomErrorController {
 
     @ExceptionHandler
     ResponseEntity handleJPAViolantions(TransactionSystemException exception){
-        return ResponseEntity.badRequest().build();
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+        if (exception.getCause() instanceof RollbackException) {
+            ConstraintViolationException ve = (ConstraintViolationException) exception.getCause().getCause();
+            List errors = ve.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                        Map<String, String> errMap = new HashMap<>();
+                        errMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                        return  errMap;
+                    }).toList();
+            return responseEntity.body(errors);
+        }
+        return responseEntity.build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIT.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
@@ -21,10 +22,12 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -63,8 +66,9 @@ class BeerControllerIT {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(beerMap)))
-                .andExpect(status().isBadRequest());
-
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.length()", is(1)))
+                .andReturn();
     }
 
     @Test
