2c7a7bf1e02669541203fdfeb8866e4515d838e1
Pedro Medeiros <pietromaddera@gmail.com>
2024-11-29 21:21:16 +0100

    156 - Update Service to Accept Query Parameters

--- src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/BeerController.java
@@ -67,7 +67,7 @@ public class BeerController {
 
     @GetMapping(BEER_PATH)
     public List<BeerDTO> listBeers(@RequestParam(required = false) String beerName) {
-        return beerService.listBeers();
+        return beerService.listBeers(beerName);
     }
 
     @GetMapping(BEER_PATH_PATH_ID)
