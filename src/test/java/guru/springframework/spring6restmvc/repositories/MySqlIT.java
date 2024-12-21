124bace7f315e3560a796f5f7c989558ae467b25
Pedro Medeiros <pietromaddera@gmail.com>
2024-12-22 00:45:49 +0100

    142. Using Maven Failsafe for Running Integrations Test

--- src/test/java/guru/springframework/spring6restmvc/repositories/MySqlIT.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/MySqlIT.java
@@ -1,18 +1,16 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 @SpringBootTest
 @ActiveProfiles("localmysql")
-public class MySqlTest {
+public class MySqlIT {
 
     @Container
     @ServiceConnection
